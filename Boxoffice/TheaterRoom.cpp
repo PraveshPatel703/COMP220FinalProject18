8e178b54da4b0a0280710603692bc5f978ffd3e2
Matthew Becker <mbecker5@ithaca.edu>
2018-12-17 23:52:01 -0500

    updated constructors for box office and theater room

--- Boxoffice/TheaterRoom.cpp
+++ Boxoffice/TheaterRoom.cpp
@@ -5,10 +5,12 @@
 #include "TheaterRoom.h"
 
 
-TheaterRoom::TheaterRoom(int seatCount, int roomNumber){
+TheaterRoom::TheaterRoom(int seatCount, int roomNumber, Movie moviePlaying){
         //this function will create the rooms< currently we will set to a max of 5
         this->seatCount = seatCount;
+        this->ticketsSold = 0;
         this->roomNumber = roomNumber;
+        this->moviePlaying = moviePlaying;
 }
 
 TheaterRoom::~TheaterRoom(){
